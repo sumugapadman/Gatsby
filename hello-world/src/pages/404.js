5422286dfcede70c392db9d4219bb2dd4ce6e55a
Sumuga <sumugapadman@yahoo.in>
2019-11-29 02:57:51 +0530

    Adding new pages

---
+++ hello-world/src/pages/404.js
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default () => {
+    return <div>
+        <h1>This is a 404 Page</h1>
+        <p>The Page you are looking for does not exist.</p>
+    </div>
+}
