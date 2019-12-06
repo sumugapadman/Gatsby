f9d4376b2815573b542a7a5bed6f191fcfd9041e
Sumuga <sumuga.padman@salesforce.com>
2019-12-06 19:27:19 +0530

    gatsby styling intro

--- hello-world/src/pages/404.js
+++ hello-world/src/pages/404.js
@@ -1,8 +1,11 @@
 import React from "react"
+import styles from './404.module.scss';
 
 export default () => {
-    return <div>
-        <h1>This is a 404 Page</h1>
-        <p>The Page you are looking for does not exist.</p>
+    return <div className={styles.content}>
+        <h1 className={styles.header}>This is a 404 Page</h1>
+        <p className={styles.errorMessage}>
+            The Page you are looking for does not exist.
+        </p>
     </div>
 }
